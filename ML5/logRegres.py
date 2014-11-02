4e14d128f65b6e44d5a98bc4770a1fde910e2906
norsd <norsd2004@gmail.com>
2014-11-02 18:50:57 +0800

    aaaa

--- ML5/logRegres.py
+++ ML5/logRegres.py
@@ -18,35 +18,35 @@ def sigmoid(inX):
 def gradAscent(dataMatIn, classLabels):
     dataMatrix = mat(dataMatIn)
     labelMat = mat(classLabels).transpose()
-    m,n = shape(dataMatrix)
+    m, n = shape(dataMatrix)
     alpha = 0.001
     maxCycles = 500
-    weights = ones((n,1))
+    weights = ones((n, 1))
     for k in range(maxCycles):
         h = sigmoid(dataMatrix*weights)
         error = (labelMat - h)
         weights = weights + alpha * dataMatrix.transpose()*error
     return weights
 
-def plotBestFit(dataMat,labelMat):
+def plotBestFit(dataMat, labelMat):
     import matplotlib.pyplot as plt
     #dataMat, labelMat= loadDataSet()
     weights = gradAscent(dataMat, labelMat).getA()
-    dataArr = array(dataMat)
-    n = shape(dataArr)[0]
+    datass = array(dataMat)
+    n = shape(datass)[0]
     xcord1 = []; ycord1 = []
     xcord2 = []; ycord2 = []
     for i in range(n):
         if int(labelMat[i]) == 1:
-            xcord1.append(dataArr[i,1]); ycord1.append(dataArr[i,2])
+            xcord1.append(datass[i, 1]); ycord1.append(datass[i, 2])
         else:
-            xcord2.append(dataArr[i,1]); ycord2.append(dataArr[i,2])
+            xcord2.append(datass[i, 1]); ycord2.append(datass[i, 2])
     fig = plt.figure()
     ax = fig.add_subplot(111)
     ax.scatter(xcord1, ycord1, s=30, c='red', marker='s')
     ax.scatter(xcord2, ycord2, s=30, c='green')
-    x=arange(-3.0, 3.0, 0.1)
-    y=(-weights[0]-weights[1]*x)/weights[2]
-    ax.plot(x,y)
+    x = arange(-3.0, 3.0, 0.1)
+    y = (-weights[0]-weights[1]*x)/weights[2]
+    ax.plot(x, y)
     plt.xlabel('X1'); plt.ylabel('X2');
     plt.show()
