168f8efe6e6e36abe396a2facdbd9800fc7e8a4d
norsd <norsd2004@gmail.com>
2015-05-09 20:05:09 +0800

    Merge branch 'master' of https://github.com/norsd/PythonProjects
    Conflicts:
    	.idea/workspace.xml

--- mstsc/mstsc.py
+++ mstsc/mstsc.py
@@ -10,9 +10,10 @@ from norlib.Socket.transfer import *
 
 
 class MstscServer(threading.Thread):
-    def __init__(self, a_exchange_ip, a_exchange_port, a_mstsc_ip, a_mstsc_port):
+    def __init__(self, a_exchange_address, a_mstsc_ip, a_mstsc_port):
+        threading.Thread.__init__(self)
         self.exchange = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.exchange.connect(a_exchange_ip, a_exchange_port)
+        self.exchange.connect(a_exchange_address)
         self.mstsc = False
         self.mstsc_ip = a_mstsc_ip
         self.mstsc_port = a_mstsc_port
@@ -21,10 +22,37 @@ class MstscServer(threading.Thread):
         while True:
             try:
                 datas = self.exchange.recv(1024)
+                if not datas:
+                    print("远端链接关闭")
+                    break
                 if not self.mstsc:
+                    print("建立mstsc")
                     self.mstsc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                    self.mstsc.connect(self.mstsc_ip, self.mstsc_port)
-                    Transfer(self.mstsc, self.exchange)
+                    self.mstsc.connect((self.mstsc_ip, self.mstsc_port))
+                    Transfer((self.mstsc, "mstsc"), (self.exchange, "exchange"), self._peer_closed, self).start()
                 self.mstsc.send(datas)
+                print("exchange=>mstsc")
             except:
-                print("Unexpected error:", sys.exc_info()[0])
+                print("Unexpected error:", sys.exc_info())
+
+    @staticmethod
+    def _peer_closed(a_exchange, a_peer):
+        print("fuck!!")
+        self = a_exchange
+        if self.peer0 == a_peer:
+            self.peer0 = False
+            print("Peer0 关闭")
+        elif self.peer1 == a_peer:
+            self.peer1 = False
+            print("Peer1 关闭")
+        else:
+            print("无法识别的Peer关闭:", a_peer)
+
+if __name__ == '__main__':
+    print('Start mstsc')
+    exchange_ip = "101.95.130.218"
+    exchange_port = 6503
+    exchange_address = "101.95.130.218:6503"
+    mstsc_ip = "10.10.40.193"
+    mstsc_port = 3389
+    MstscServer((exchange_ip, exchange_port), mstsc_ip, mstsc_port).start()
--- norlib/Socket/transfer.py
+++ norlib/Socket/transfer.py
@@ -3,7 +3,8 @@
 __author__ = 'norsd@163.com'
 
 import sys
-import socket
+from socket import error as SocketError
+import errno
 import time
 import threading
 
@@ -11,8 +12,19 @@ import threading
 class Transfer(threading.Thread):
     def __init__(self, source, sink, fn, fn_para):
         threading.Thread.__init__(self)
-        self.source = source
-        self.sink = sink
+        if source is tuple:
+            self.source = source[0]
+            self.sourcename = source[1]
+        else:
+            self.source = source
+            self.sourcename = source.getpeername()
+
+        if sink is tuple:
+            self.sink = sink[0]
+            self.sinkname = sink[1]
+        else:
+            self.sink = sink
+            self.sinkname = "sink"
         self.fn_peer_closed = fn
         self.fn_peer_closed_para = fn_para
 
@@ -27,6 +39,11 @@ class Transfer(threading.Thread):
                     print("没有远端peer可以转发数据,数据被忽略")
                 else:
                     self.sink.send(data)
+                    print(self.sourcename, "=>", self.sinkname)
+            except SocketError as e:
+                if e.errno == errno.ECONNRESET:
+                    self.fn_peer_closed(self.fn_peer_closed_para, self.source)
+                    break
             except:
                 print("Transfer ", self.source.getpeername(), " Unexpected error:", sys.exc_info())
                 break
