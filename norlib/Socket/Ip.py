92765d1be3df41d33ec67c44edd4884d0e01fb22
norsd <norsd@163.com>
2015-11-21 13:48:08 +0800

    aa

--- norlib/Socket/Ip.py
+++ norlib/Socket/Ip.py
@@ -10,3 +10,8 @@ def GetIps():
     # 返回主机名,主机别名列表,主机IP地址列表
     infos = socket.gethostbyname_ex(socket.gethostname())
     return infos[2]
+
+def get_address_string(a_sock):
+    infos = a_sock.getpeername()
+    address = "%s:%s" % (infos[0], infos[1])
+    return address
