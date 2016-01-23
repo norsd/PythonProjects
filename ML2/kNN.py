d655c42dc1756ed9a75da962290ec5ae4e8cdb3c
norsd <norsd@163.com>
2016-01-24 03:17:51 +0800

    Add ML2

--- ML2/kNN.py
+++ ML2/kNN.py
@@ -9,6 +9,15 @@ def classify0(inx, data_set, labels, k):
     data_set_size = data_set.shape[0]
     # numpy.tile
     diff_mat = tile(inx, (data_set_size, 1)) - data_set
+    sq_diff_mat = diff_mat ** 2
+    sq_distances = sq_diff_mat.sum(axis=1)
+    sorted_dist_indicies = sq_distances.argsort()
+    class_count = {}
+    for i in range(k):
+        vote_i_label = labels[sorted_dist_indicies[i]]
+        class_count[vote_i_label] = class_count.get(vote_i_label, 0) + 1
+    sorted_class_count = sorted(class_count.items(), key=operator.itemgetter(1), reverse=True)
+    return sorted_class_count[0][0]
 
 
 def create_data_set():
