b448dc895d710949ba41ab52ee79684134a3b98a
norsd <norsd@163.com>
2016-02-09 01:44:56 +0800

    Update ML2

--- ML2/kNN.py
+++ ML2/kNN.py
@@ -2,16 +2,18 @@
 from matplotlib.font_manager import FontProperties
 from numpy import *
 import matplotlib.pyplot as plt
+from typing import List
 import os
 import operator
 import zipfile
+# 为matplotlib显示中文
 font = FontProperties(fname=os.path.expandvars(r"%windir%\fonts\simsun.ttc"), size=14)
 
 
 __author__ = 'norsd@163.com'
 
 
-def classify0(inx, data_set, labels, k):
+def classify0(inx: ndarray, data_set: ndarray, labels: list, k: int):
     """
     [data0, data1, ...]
     与
@@ -70,25 +72,34 @@ def classify0(inx, data_set, labels, k):
     return sorted_class_count[0][0]
 
 
-def create_data_set():
-    group = array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
-    labels = ['A', 'A', 'B', 'B']
-    return group, labels
-
-
-
-# 文件格式如下:
-# #
-# 40920	8.326976	0.953952	largeDoses
-# 14488	7.153469	1.673904	smallDoses
-# 26052	1.441871	0.805124	didntLike
-# #
-# 每项内容分别为: 飞行常客里程数, 玩视屏游戏消耗时间, 每周消费冰淇淋公升数，关注度
-def file_to_matrix(a_filename):
+def auto_norm(set_data)-> tuple[ndarray, float, float]:
     """
-    将一个特定格式文件转化为2个matrix
-    @param a_filename:
+    把数据集归一
+    @param set_data:
     @return:
+    """
+    min_value = set_data.min()
+    max_value = set_data.max()
+    ranges = max_value - min_value
+    set_norm = zeros(shape(set_data))
+    m = set_data.shape[0]
+    set_norm = set_data - tile(min_value, (m, 1))
+    return set_norm, ranges, min_value
+
+
+def file_to_matrix(a_filename: str)-> tuple[ndarray, list, dict, list[str]]:
+    """
+    将一个特定格式文件转化为matrix
+                    40920	        8.326976	        0.953952	        largeDoses
+                    14488	        7.153469	        1.673904	        smallDoses
+                    26052	        1.441871	        0.805124	        didntLike
+    每项内容分别为: 飞行常客里程数, 玩视屏游戏消耗时间, 每周消费冰淇淋公升数，关注度
+    @param a_filename: 文件名
+    @return:
+        ret_mat     数行数据,举例第i行为[40920, 8.326976, 0.953952]，Columns分别表示"飞行常客里程数", "玩视屏游戏消耗时间", "每周消费冰淇淋公升数"
+        vt_label    对每一行数据的一个分类,注意分类用数字表示，例如i行的数据类别为：LargeDoses, 在vt_label中的第i个就记录为1
+        ret_descs   Dictionary<string,int> 用于转换类似 "LargeDoses"->1
+        ["飞行常客里程数", "玩视屏游戏消耗时间", "每周消费冰淇淋公升数"]
     """
     fr = open(a_filename)
     f_lines = fr.readlines()
@@ -112,25 +123,49 @@ def file_to_matrix(a_filename):
     return ret_mat, vt_label, ret_descs, ["飞行常客里程数", "玩视屏游戏消耗时间", "每周消费冰淇淋公升数"]
 
 
-# 添加data到dictionary中当key尚未存在时
-def set_value(self, key, data):
-    if key not in self:
-        self[key] = data
+def set_value(a_dict: dict, a_key, a_value):
+    """
+    当key尚未存在时,添加data到dictionary中
+    @param a_dict:
+    @param a_key:
+    @param a_value:
+    @return:
+    """
+    if a_key not in a_dict:
+        a_dict[a_key] = a_value
     return
 
 
-def auto_norm(set_data):
-    min_value = set_data.min()
-    max_value = set_data.max()
-    ranges = max_value - min_value
-    set_norm = zeros(shape(set_data))
-    m = set_data.shape[0]
-    set_norm = set_data - tile(min_value, (m, 1))
-    return set_norm, ranges, min_value
+def extract_zip(a_target_dir):
+    """
+    解压digits.zip到a_path中
+    @param a_target_dir:
+    """
+    z = zipfile.ZipFile("digits.zip", "r")
+    name_list = z.namelist()
+    for n in name_list:
+        str_name = str(n)
+        path = "{0}/{1}".format(a_target_dir, str_name)
+        if str_name[-1] == '/':
+            if not os.path.exists(path):
+                os.mkdir(path)
+        else:
+            if not os.path.exists(path):
+                bytes = z.read(str_name)
+                f = open(path, 'wb')
+                f.write(bytes)
+                f.close()
+            image_to_vector(path)
 
 
-#
 def dating_class_test():
+    """
+    测试文件datingTestSet.txt中的数据
+    拿文件中10%的数据作为待测数据
+    剩余90%作为已知样本
+    计算10%的数据的最后归类与实际10%数据的类别比较
+    打印出这10%的数据计算类别的准确率
+    """
     hot_ratio = 0.1
     mat_dating, vt_labels, c, d = file_to_matrix("datingTestSet.txt")
     mat_norm, ranges, min_values = auto_norm(mat_dating)
@@ -145,13 +180,59 @@ def dating_class_test():
     print("The total error rate is: %f", error_count/float(num_test_vecs))
 
 
-def read_zip(a_relative_path):
-    z = zipfile.ZipFile("digits.zip", "r")
-    print(z.namelist())
+def number_recognize():
+    os.chdir('C:\\GitHubRepositories\\PythonProjects\\ML2')
+    extract_zip("c:/")
+    traning_folder = "c:/trainingDigits"
+    filenames =  os.listdir(traning_folder)
+    traning_matrix = zeros((len(filenames), 32*32))
+    traning_labels = []
+    for i in range(0, len(filenames)):
+        filename = filenames[i]
+        label = filename[0] # 文件名形如 4_129.txt 表示字符"4"的第129个样本
+        vt = image_to_vector("{0}/{1}".format(traning_folder, filename))
+        traning_matrix[ i,: ] = vt
+        traning_labels.append(label)
+    test_folder = "c:/testDigits"
+    filenames = os.listdir(test_folder)
+    error_count = 0
+    for i in range(0, len(filenames)):
+        filename = filenames[i]
+        label_expected = filename[0] # 文件名形如 4_129.txt 表示字符"4"的第129个测试样本
+        vt = image_to_vector("{0}/{1}".format(traning_folder, filename))
+        label_calculate = classify0(vt, traning_matrix, traning_labels, 3)
+        if label_expected != label_calculate:
+            print("{0}被错认为是{1}".format(filename, label_calculate))
+            error_count += 1
+    print("Error count", error_count)
+    print("错误率:{0}%".format(round(error_count*100/len(filenames), 2)))
+
+
+def image_to_vector(a_file_path):
+    """
+    必须是32*32的图片
+    @param a_file_path:
+    @return:
+    """
+    f = open(a_file_path, 'r')
+    lines = f.readlines()
+    if len(lines) != 32:
+        print("高度不合法的文件:", a_file_path)
+        return
+    vt = zeros((1, 32*32))
+    for i in range(0, len(lines)):
+        line = list(lines[i])[:32]
+        if len(line) != 32:
+            print("宽度({0})不合法的文件:{1}".format(len(line), a_file_path))
+            return
+        for j in range(0, 32):
+            vt[0, i*32 + j] = line[j]
+    return vt
 
 
 # 快速运行函数
-def run_helper():
+# 绘画数据图
+def run_draw_dating():
     import os
     os.chdir('C:\\GitHubRepositories\\PythonProjects\\ML2')
     a, label_numbers, dt_desc, columns = file_to_matrix("datingTestSet.txt")
@@ -182,5 +263,15 @@ def run_helper():
     plt.show()
 
 
+# region Obsolete functions
+def create_data_set():
+    group = array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
+    labels = ['A', 'A', 'B', 'B']
+    return group, labels
+# endregion
+
+
 if __name__ == '__main__':
-    run_helper()
+    run_draw_dating()
+
+
